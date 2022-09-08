21fcf4482ad88d1a32890019d1cbc01891785861
epb <evanpbishop@gmail.com>
2022-09-08 18:49:44 -0400

    better lsp-mode configuration

--- init.el
+++ init.el
@@ -28,7 +28,16 @@
 ;; COMplete ANYthing
 (use-package company
   :init (add-hook 'after-init-hook 'global-company-mode))
-(use-package lsp-mode)
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (
+         (c-mode . lsp)
+	 (python-mode . lsp)
+         ;; if you want which-key integration
+         ;;(lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
 ;; -------------------------- ;;
 
